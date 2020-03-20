0eecf6ea6c4e54fb92defd2ae97c37ec0c2a5b5b
andres <andresfmoya@gmail.com>
2020-03-20 16:48:55 -0500

    testing Continuous Deployment to Heroku

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,11 @@ GIT_VERSION=$(git describe --always --abbrev --tags --long) # Git hash and tags
 # Build and tag image
 docker build -t ${IMAGE}:${GIT_VERSION} .
 docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
+docker tag ${IMAGE}:${GIT_VERSION} registry.heroku.com/$HEROKU_APP_NAME/web
 
 # Log in to Docker Hub and push
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push ${IMAGE}:${GIT_VERSION}
+echo "${HEROKU_PASSWORD}" | docker login -u "${HEROKU_USERNAME}" --password-stdin registry.heroku.com
+docker push ${IMAGE}:${GIT_VERSION};
+docker push registry.heroku.com/$HEROKU_APP_NAME/web;
+heroku container:release web --app $HEROKU_APP_NAME
